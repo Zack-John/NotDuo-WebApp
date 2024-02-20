28aa9152193c6a5bc39bd5df16d0207f3c50dcc9
Zack <johnsonzack94@gmail.com>
2024-02-20 13:30:32 -0800

    database get/set working

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 // https://firebase.google.com/docs/web/learn-more#libraries-cdn
 */
 
-// Import the functions we need from the SDKs we need
+// Import the functions we need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getDatabase, set, get, update, remove, ref, child} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,13 +19,56 @@ const firebaseConfig = {
   appId: "1:644883802257:web:93ec198aa9ed7141597730"
 };
 
-// Initialize Firebase
+// Initialize & Get Ref to Firebase App, Database
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
 
-// TESTING: print db info to make sure its hooked up
-// console.log(database);
+var enterUsername = document.querySelector("#enterUsername");
+var enterFirstname = document.querySelector("#enterFirstname");
+var enterLastname = document.querySelector("#enterLastname");
+var enterPassword = document.querySelector("#enterPassword");
 
-// setup variables
+var findUsername = document.querySelector("#findUsername");
+var findFirstname = document.querySelector("#findFirstname");
+var findLastname = document.querySelector("#findLastname");
+var findEmail = document.querySelector("#findEmail");
+
+var insertButton = document.querySelector("#insertButton");
+var findButton = document.querySelector("#findButton");
+
+function InsertData() {
+  set(ref(database, "Users/" + enterUsername.value), {
+    firstName: enterFirstname.value,
+    lastName: enterLastname.value,
+    passwordHash: enterPassword.value,
+    username: enterUsername.value
+  })
+  .then(()=>{
+    alert("Data inserted into the database!")
+  })
+  .catch((error)=>{
+    alert(error)
+  });
+}
+
+function FindData() {
+  var dbref = ref(database);
+
+  get(child(dbref, "Users/" + findUsername.value))
+  .then((snapshot)=>{
+    if (snapshot.exists()) {
+      findFirstname.innerHTML = snapshot.val().firstName;
+      findLastname.innerHTML = snapshot.val().lastName;
+    }
+    else { alert("No data found :("); }
+  })
+  .catch((error)=>{
+    alert(error);
+  })
+}
+
+// TODO: hash passwords
+
+// add button event listeners
+insertButton.addEventListener('click', InsertData);
+findButton.addEventListener('click', FindData);
